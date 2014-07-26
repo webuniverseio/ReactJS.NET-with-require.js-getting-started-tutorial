50b2722338dc6bf615be51aa9cbaadb80178d610
Rick Beerendonk <rick@beerendonk.com>
2014-07-26 13:00:01 +0200

    Your first component

--- ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
+++ ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
@@ -1,1 +1,16 @@
-﻿
+﻿/** @jsx React.DOM */
+
+var CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+				Hello, world! I am a CommentBox.
+			</div>
+		);
+	}
+});
+
+React.renderComponent(
+	<CommentBox />,
+	document.getElementById('content')
+);
