f4811f0e1e42a93c6d87a000fc1d422d29ab04ce
Rick Beerendonk <rick@beerendonk.com>
2014-07-26 14:03:13 +0200

    Component properties & using props

--- ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
+++ ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
@@ -1,8 +1,23 @@
-﻿var CommentList = React.createClass({
+﻿var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		);
+	}
+});
+
+var CommentList = React.createClass({
 	render: function() {
 		return (
 			<div className="commentList">
-				Hello, world! I am a CommentList.
+				<Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
+				<Comment author="Pete Hunt">This is one comment</Comment>
+				<Comment author="Jordan Walke">This is *another* comment</Comment>
 			</div>
 		);
 	}
