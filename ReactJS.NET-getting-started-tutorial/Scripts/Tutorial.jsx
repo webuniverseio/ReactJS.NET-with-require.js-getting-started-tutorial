913d8123243cb7f666ad74b12e0eb60a595663e7
Rick Beerendonk <rick@beerendonk.com>
2014-07-26 13:01:32 +0200

    Composing components

--- ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
+++ ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
@@ -1,10 +1,32 @@
 ﻿/** @jsx React.DOM */
 
+var CommentList = React.createClass({
+	render: function() {
+		return (
+			<div className="commentList">
+				Hello, world! I am a CommentList.
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function() {
+		return (
+			<div className="commentForm">
+				Hello, world! I am a CommentForm.
+			</div>
+		);
+	}
+});
+
 var CommentBox = React.createClass({
 	render: function() {
 		return (
 			<div className="commentBox">
-				Hello, world! I am a CommentBox.
+				<h1>Comments</h1>
+				<CommentList />
+				<CommentForm />
 			</div>
 		);
 	}
