a040568580e889a80cad1498c831ae37af4fd4f5
Rick Beerendonk <rick@beerendonk.com>
2014-07-26 14:14:52 +0200

    Hook up the data model

--- ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
+++ ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
@@ -1,5 +1,11 @@
 ﻿/** @jsx React.DOM */
 
+var data = [
+  { Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
+  { Author: "Pete Hunt", Text: "This is one comment" },
+  { Author: "Jordan Walke", Text: "This is *another* comment" }
+];
+
 var converter = new Showdown.converter();
 
 var Comment = React.createClass({
@@ -18,11 +24,12 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
 	render: function() {
+		var commentNodes = this.props.data.map(function (comment) {
+			return <Comment author={comment.Author}>{comment.Text}</Comment>;
+		});
 		return (
 			<div className="commentList">
-				<Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
-				<Comment author="Pete Hunt">This is one comment</Comment>
-				<Comment author="Jordan Walke">This is *another* comment</Comment>
+				{commentNodes}
 			</div>
 		);
 	}
@@ -43,7 +50,7 @@ var CommentBox = React.createClass({
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList />
+				<CommentList data={this.props.data} />
 				<CommentForm />
 			</div>
 		);
@@ -51,6 +58,6 @@ var CommentBox = React.createClass({
 });
 
 React.renderComponent(
-	<CommentBox />,
+	<CommentBox data={data} />,
 	document.getElementById('content')
 );
