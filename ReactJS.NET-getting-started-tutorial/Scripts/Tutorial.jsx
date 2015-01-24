4cbdd7bf428f1d658bcb4c64a1a234ad8e2f09ab
Rick Beerendonk <rick@beerendonk.com>
2015-01-24 23:59:40 +0100

    Adding Markdown

--- ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
+++ ReactJS.NET-getting-started-tutorial/Scripts/Tutorial.jsx
@@ -1,11 +1,13 @@
 ﻿var Comment = React.createClass({
 	render: function() {
+		var converter = new Showdown.converter();
+		var rawMarkup = converter.makeHtml(this.props.children.toString());
 		return (
 			<div className="comment">
 				<h2 className="commentAuthor">
 					{this.props.author}
 				</h2>
-				{this.props.children}
+				<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
 			</div>
 		);
 	}
