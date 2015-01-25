38509644d537ed41649a4c3e2d11f416436bfa7d
Rick Beerendonk <rick@beerendonk.com>
2015-01-25 03:07:27 +0100

    Project setup (ReactJS.NET v1.3)

---
+++ ReactJS.NET-getting-started-tutorial/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
